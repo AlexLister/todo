977bd9e0b139a5bd558fcb0392ac9853e6e8f569
Andrew Charlton <a.charlton32@gmail.com>
2017-02-28 12:07:08 +0000

    Rendering template to file

--- todo.py
+++ todo.py
@@ -1,7 +1,19 @@
 import web
 
-TASKS = ["Learn to create templates", "Learn to populate them using jinja2"]
+TASKS = ["Learn to create templates", "Learn to populate them"]
 
 
 
+def render_tasklist():
+
+    render = web.template.render('templates/')
+    return render.tasklist(TASKS)
+
+
+
+if __name__ == "__main__":
+
+    file = open("tasklist.html", "w")
+    file.write(str(render_tasklist()))
+    file.close()
 
